16a91acd07ac8786a805c5d794643bcfee007abd
Florian Grosdidier <florian.grosdidier@gmail.com>
2023-07-26 14:46:15 +0200

    removing excess

--- app/persistence/PoemRepository.php
+++ app/persistence/PoemRepository.php
@@ -13,19 +13,16 @@ class PoemRepository implements IObtainPoems
 
   public function addPoem(Poem $poem)
   {
-    // Add a new poem to the repository
     $this->poems[$poem->getId()] = $poem;
   }
 
   public function getPoemById($poemId)
   {
-    // Find and return the poem by its ID
     return isset($this->poems[$poemId]) ? $this->poems[$poemId] : null;
   }
 
   public function getAllPoems()
   {
-    // Return all poems in the repository
     return $this->poems;
   }
 }
